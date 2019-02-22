cb58b82429fc923beb604fc01ca3c550cad774e0
finchmgn <ffinch@mail.ru>
2019-02-22 12:40:03 +0200

    add carousel styles

--- app/js/common.js
+++ app/js/common.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
-    $('.your-class').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
+    $(".owl-carousel").owlCarousel({
+        center: true,
+        items:1,
+        loop:true,
+        stagePadding: 15,
+        margin:10,
+        responsive:{
+            992:{
+                items:2,
+                stagePadding: 15
+            },
+            1366:{
+                items:3,
+                stagePadding: 50,
+            }
+        }
     });
-  });
+});
